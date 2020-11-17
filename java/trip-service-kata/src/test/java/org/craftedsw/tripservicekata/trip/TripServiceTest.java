88ca9cfc6bf6457461cf62eb4b5e7f00b00e6188
“Paul <paul.ziwiakowsky@octo.com>
2020-11-17 10:55:56 +0100

    first draft

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,51 @@
 package org.craftedsw.tripservicekata.trip;
 
+import com.sun.tools.javac.code.Attribute;
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
 public class TripServiceTest {
-	
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        user = null;
+    }
+
+    @Test
+    void getTripsByUserWhenNoFriendsShouldReturnEmptyTripList() {
+        // Given
+        TripService tripService = new TripServiceForTest();
+        user = new User();
+
+        // When
+        List<Trip> trips = tripService.getTripsByUser(tripService.getLoggedUser());
+
+        // Then
+        Assertions.assertEquals(trips, Collections.emptyList());
+    }
+
+    @Test
+    void getTripsByUserWhenUserNotLoggedShouldThrowException() {
+        // Given
+        TripService tripService = new TripServiceForTest();
+
+        // Then
+        Assertions.assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+    }
+
+    public class TripServiceForTest extends TripService {
+
+        @Override
+        protected User getLoggedUser() {
+            return user;
+        }
+    }
 }
